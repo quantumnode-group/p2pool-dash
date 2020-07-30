05557bc92fe5afc14cff964ff97ced27c70b26fa
Bryte Morio <brytemorio@qngnode.cc>
2020-07-30 07:03:35 +0100

    updates

--- p2pool/data.py
+++ p2pool/data.py
@@ -655,7 +655,7 @@ def get_warnings(tracker, best_share, net, qnodecoind_getnetworkinfo, qnodecoind
     majority_desired_version = max(desired_version_counts, key=lambda k: desired_version_counts[k])
     if majority_desired_version > (Share.SUCCESSOR if Share.SUCCESSOR is not None else Share).VOTING_VERSION and desired_version_counts[majority_desired_version] > sum(desired_version_counts.itervalues())/2:
         res.append('A MAJORITY OF SHARES CONTAIN A VOTE FOR AN UNSUPPORTED SHARE IMPLEMENTATION! (v%i with %i%% support)\n'
-            'An upgrade is likely necessary. Check https://github.com/qnodecoinpay/p2pool-qnodecoin for more information.' % (
+            'An upgrade is likely necessary. Check https://github.com/quantumnode-group/qnodecoin-p2pool for more information.' % (
                 majority_desired_version, 100*desired_version_counts[majority_desired_version]/sum(desired_version_counts.itervalues())))
     
     if qnodecoind_getnetworkinfo['warnings'] != '':
--- p2pool/networks/qnodecoin.py
+++ p2pool/networks/qnodecoin.py
@@ -12,9 +12,9 @@ COINBASEEXT = '0D2F5032506F6F6C2D444153482F'.decode('hex')
 P2P_PORT = 20612
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
-PERSIST = True
+PERSIST = False
 WORKER_PORT = 7903
 #BOOTSTRAP_ADDRS = 'qnodecoin01.p2poolmining.us p2pool.2sar.ru qnodecoin02.p2poolmining.us p2pool.qnodecoin.siampm.com qnodecoin03.p2poolmining.us crypto.office-on-the.net qnodecoin04.p2poolmining.us'.split(' ')
-BOOTSTRAP_ADDRS = 'p2pool.2sar.ru  crypto.office-on-the.net'.split(' ')
+BOOTSTRAP_ADDRS = 'node1.walletbuilders.com 140.82.9.137 154.223.134.122 47.245.0.245 45.76.211.195 110.232.115.241 174.138.33.59 149.28.59.152'.split(' ')
 ANNOUNCE_CHANNEL = '#qnodecoin-p2pool'
 VERSION_CHECK = lambda v: v >= 120100
