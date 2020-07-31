5020c4a3876dc188175dbb8d0dbe69bc866e93f8
Bryte Morio <brytemorio@qngnode.cc>
2020-07-31 13:25:15 +0100

    changed port

--- p2pool/networks/qnodecoin.py
+++ p2pool/networks/qnodecoin.py
@@ -9,15 +9,15 @@ SPREAD = 10 # blocks
 IDENTIFIER = '7242ef345e1bed6b'.decode('hex')
 PREFIX =  '3b3e1286f446b891'.decode('hex')
 COINBASEEXT =  '0D2F5032506F6F6C2D444153482F'.decode('hex')
-P2P_PORT = 20612
+P2P_PORT = 8500
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = False
-WORKER_PORT = 7903
+WORKER_PORT = 6500
 
 #BOOTSTRAP_ADDRS = 'qnodecoin01.p2poolmining.us p2pool.2sar.ru qnodecoin02.p2poolmining.us p2pool.qnodecoin.siampm.com qnodecoin03.p2poolmining.us crypto.office-on-the.net qnodecoin04.p2poolmining.us'.split(' ')
 
 BOOTSTRAP_ADDRS = '140.82.9.137 154.223.134.122 47.245.0.245 45.76.211.195 110.232.115.241 174.138.33.59 149.28.59.152'.split(' ')
-
+#BOOTSTRAP_ADDRS = 'p2pool.2sar.ru crypto.office-on-the.net'.split(' ')
 ANNOUNCE_CHANNEL = '#qnodecoin-p2pool'
 VERSION_CHECK = lambda v: v >= 120100
