31aa04ee05bf74f61c2b2dd215c4773f7f291ace
Bryte Morio <brytemorio@qngnode.cc>
2020-07-31 05:06:48 +0100

    updates

---
+++ web-static/js/config.js
@@ -0,0 +1,12 @@
+var config = {
+  myself: [],
+  host: '',
+  // data reload interval in seconds
+  reload_interval: 30,
+  // chart reload interval in seconds
+  reload_chart_interval: 600,
+  // HTML to load at top of page
+  //header_content_url: "http://localhost:8888/custom_content.html",
+  // Default Theme
+  theme: 'Cyborg'
+}
